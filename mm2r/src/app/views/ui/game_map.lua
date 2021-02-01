b2389b5fddedd91fe9092ca32e7408d57c2d780f
coffeeqdy <464947068@qq.com>
2021-02-01 22:32:06 +0800

    /mm2r:

--- mm2r/src/app/views/ui/game_map.lua
+++ mm2r/src/app/views/ui/game_map.lua
@@ -4,8 +4,8 @@ local global_define = require("app.public.global.global_define")
 local ui_observer = require("app.public.util.ui_observer")
 
 local g_move_size = 32--每格的距离
-local g_view_width = 672--11格
-local g_view_height = 672--11格
+local g_view_width = 352--11格
+local g_view_height = 352--11格
 
 local game_map = class("game_map", cc.Node)
 
@@ -15,7 +15,7 @@ function game_map:ctor()
     local _cache_col = g_view_width / g_move_size + 4--左右各预留2格
     local _cache_row = g_view_height / g_move_size + 4--上下各预留2格
     
-    local _begin_pos_lt = cc.p(-(_cache_col - 1) / 2, -(_cache_row - 1) / 2)--左下角初始角
+    local _begin_pos_lt = cc.p(-(_cache_col - 1) * g_move_size / 2, -(_cache_row - 1) * g_move_size / 2)--左下角初始角
 
     self.table_squares = {}
     for i = 1, _cache_col do
