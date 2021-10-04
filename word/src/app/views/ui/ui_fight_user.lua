f820f649ea1e4b22c06ab724ced186e90e567a65
coffee <464947068@qq.com>
2021-10-04 20:40:28 +0800

    初版ui接入

--- word/src/app/views/ui/ui_fight_user.lua
+++ word/src/app/views/ui/ui_fight_user.lua
@@ -1,7 +1,10 @@
 local ui_fight_user = class("ui_fight_user", cc.Node)
 
 function ui_fight_user:ctor()
-    self.node_root = cc.CSLoader:createNode()
+    self.node_root = cc.CSLoader:createNode("scene/ui_fight_user.csb")
+
+    self.loading_bar = self.node_root:getChildByName("loading_bar")
+    self.text_info = self.node_root:getChildByName("text_info")
 end
 
 return ui_fight_user
--- word/src/config.lua
+++ word/src/config.lua
@@ -13,14 +13,14 @@ CC_DISABLE_GLOBAL = false
 
 -- for module display
 CC_DESIGN_RESOLUTION = {
-    width = 960,
-    height = 640,
-    autoscale = "FIXED_HEIGHT",
+    width = 720,
+    height = 1280,
+    autoscale = "SHOW_ALL",--FIXED_HEIGHT
     callback = function(framesize)
         local ratio = framesize.width / framesize.height
         if ratio <= 1.34 then
             -- iPad 768*1024(1536*2048) is 4:3 screen
-            return {autoscale = "FIXED_WIDTH"}
+            return {autoscale = "SHOW_ALL"}--FIXED_WIDTH
         end
     end
 }
